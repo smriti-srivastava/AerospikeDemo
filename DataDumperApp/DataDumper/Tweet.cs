cfe538a1da257a6f633c006937f191dfa009689c
Smriti Srivastava <ssrivastava@tavisca.com>
2018-09-27 21:05:00 +0530

    Added Data Dumper(Console App) using Aerospike

---
+++ DataDumperApp/DataDumper/Tweet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDumper
+{
+    class Tweet
+    {
+        public string author { get; set; }
+        public string content { get; set; }
+        public string region { get; set; }
+        public string language { get; set; }
+        public string tweet_date { get; set; }
+        public string tweet_time { get; set; }
+        public string year { get; set; }
+        public string month { get; set; }
+        public string hour { get; set; }
+        public string minute { get; set; }
+        public int following { get; set; }
+        public int followers { get; set; }
+        public string post_url { get; set; }
+        public string post_type { get; set; }
+        public int retweet { get; set; }
+        public string tweet_id { get; set; }
+        public string author_id { get; set; }
+        public string account_category { get; set; }
+
+    }
+}
